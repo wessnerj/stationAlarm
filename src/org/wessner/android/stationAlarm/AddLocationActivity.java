a52b88861f037249d2658315980ec99847f128ae
Joseph Wessner <joseph@wessner.org>
2014-11-23 21:51:29 +0100

    MapView no with zoom enabled

--- src/org/wessner/android/stationAlarm/AddLocationActivity.java
+++ src/org/wessner/android/stationAlarm/AddLocationActivity.java
@@ -1,16 +1,50 @@
 package org.wessner.android.stationAlarm;
 
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.Projection;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.OverlayManager;
+
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 
 public class AddLocationActivity extends Activity {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_location);
+		
+		MapView v = (MapView) findViewById(R.id.mapview);
+		v.setMultiTouchControls(true);
+	    v.setClickable(true);
+	    
+	    Overlay touchOverlay = new Overlay(this) {
+			@Override
+			protected void draw(Canvas arg0, MapView arg1, boolean arg2) {
+								
+			}
+			
+			@Override
+		    public boolean onLongPress(MotionEvent e, MapView mapView) {
+				Projection proj = mapView.getProjection();
+				GeoPoint loc = (GeoPoint) proj.fromPixels((int) e.getX(), (int) e.getY());
+				Log.d("Location", "Long: " + loc.getLongitude() + " Lat: " + loc.getLatitude());
+			
+				return true;
+		    }
+		};
+		
+		OverlayManager om = v.getOverlayManager();
+		om.add(touchOverlay);
 	}
 
 	@Override
