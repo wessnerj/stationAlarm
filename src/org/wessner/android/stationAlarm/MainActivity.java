6b18e3d1b66d19b22bf890e7610dce7b58697907
Joseph Wessner <joseph@wessner.org>
2015-01-02 18:32:43 +0100

    Some cleanup

--- src/org/wessner/android/stationAlarm/MainActivity.java
+++ src/org/wessner/android/stationAlarm/MainActivity.java
@@ -7,7 +7,6 @@ import org.wessner.android.stationAlarm.data.StationManager;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -22,22 +21,56 @@ import android.widget.Button;
 import android.widget.GridView;
 
 
-public class MainActivity extends Activity implements OnItemLongClickListener {
+public class MainActivity extends Activity implements OnItemLongClickListener, OnClickListener {
 	
 	/**
-	 * Request code for switching to AddFavoriteActivity
+	 * Request code for switching to AddStationActivity
 	 */
 	private static final int REQUEST_CODE = 10;
+	/**
+	 * Request code for switching to AddStationActivity
+	 */
+	private static final int REQUEST_CODE_EDIT = 20;	
 	
+	/**
+	 * Context menu id for (de)activate entry
+	 */
 	private static final int CONTEXT_ID_DE_ACTIVATE = 1;
+	/**
+	 * Context menu id for delete entry
+	 */
 	private static final int CONTEXT_ID_DELETE = 2;
+	/**
+	 * Context menu id for edit entry
+	 */
+	private static final int CONTEXT_ID_EDIT = 3;
 	
+	/**
+	 * StationManager used for retrieving the saved stations
+	 */
 	private StationManager stationManager;
+	/**
+	 * StationAdapter as middle-ware between GridView and station Manager
+	 */
 	private StationAdapter stationAdapter;
 	
+	/**
+	 * Activity's layout
+	 */
+	private View layout;
+	
+	/**
+	 * GridView showing all stations
+	 */
 	private GridView gridView;
 	
+	/**
+	 * Button for starting the alarm service
+	 */
 	private Button startButton;
+	/**
+	 * Button for stopping the alarm service
+	 */
 	private Button stopButton;
 
     @Override
@@ -45,34 +78,48 @@ public class MainActivity extends Activity implements OnItemLongClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        this.stationManager = new StationManager(new DataBaseHelper(this));
-        
-        this.startButton = (Button) findViewById(R.id.button1);
-        startButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startService();
-			}
-        });
-        this.stopButton = (Button) findViewById(R.id.button2);
+        // Get references to used view elements and register listeners       
+        this.layout = findViewById(R.id.linear_layout);
+        this.startButton = (Button) findViewById(R.id.button_start);
+        this.startButton.setOnClickListener(this);
+        this.stopButton = (Button) findViewById(R.id.button_stop);
+        this.stopButton.setOnClickListener(this);
         
         this.gridView = (GridView) findViewById(R.id.gridView1);
+        this.stationManager = new StationManager(new DataBaseHelper(this));
         this.stationAdapter = new StationAdapter(this, this.stationManager.getAllCursor());
         this.gridView.setAdapter(this.stationAdapter);
         this.gridView.invalidate();
-        // this.gridView.setOnItemLongClickListener(this);
         
+        // Add app's context menu
         registerForContextMenu(this.gridView);
     }
     
+    @Override
     protected void onResume() {
     	super.onResume();
     	
-        this.stationAdapter.changeCursor(this.stationManager.getAllCursor());
-        this.stationAdapter.notifyDataSetChanged();
-    	this.gridView.invalidate();
+    	// Update the grid view
+    	this.updateGridView();
     	
-    	if (LocationMonitorService.isRunning()) {
+    	// Change start/stop button accordingly
+    	this.setServiceButtons();
+    }
+    
+    /**
+     * Sets the visibility of the start & end service button regarding the current service state.
+     */
+    private void setServiceButtons() {
+    	this.setServiceButtons(LocationMonitorService.isRunning());
+    }
+    
+    /**
+     * Sets the visibility of the start & end service button regarding the current service state.
+     * 
+     * @param running		set to true if service is running
+     */
+    private void setServiceButtons(boolean running) {
+    	if (running) {
     		this.startButton.setVisibility(View.GONE);
     		this.stopButton.setVisibility(View.VISIBLE);
     	}
@@ -80,6 +127,8 @@ public class MainActivity extends Activity implements OnItemLongClickListener {
     		this.startButton.setVisibility(View.VISIBLE);
     		this.stopButton.setVisibility(View.GONE);
     	}
+    	
+    	this.layout.invalidate();
     }
 
     @Override
@@ -96,9 +145,9 @@ public class MainActivity extends Activity implements OnItemLongClickListener {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_add) {
-        	final Intent switchToAddFavourite = new Intent(
+        	final Intent switchToAddStation = new Intent(
 					this, AddLocationActivity.class);
-			startActivityForResult(switchToAddFavourite, REQUEST_CODE);
+			startActivityForResult(switchToAddStation, REQUEST_CODE);
 
             return true;
         }
@@ -106,20 +155,31 @@ public class MainActivity extends Activity implements OnItemLongClickListener {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Starts the Alarm service
+     */
     private void startService() {
     	Intent mServiceIntent = new Intent(this, LocationMonitorService.class);
     	this.startService(mServiceIntent);
+    	
+    	// update buttons visibility
+    	this.setServiceButtons(true);
     }
-
-	@Override
-	public boolean onItemLongClick(AdapterView<?> parent, View view,
-			int position, long id) {
-		if (R.id.gridView1 == parent.getId()) {
-			Log.d("Clicked", "gridView");
-		}
-		return false;
-	}
+    
+    /**
+     * Stops the Alarm service
+     */
+    private void stopService() {
+    	Intent mServiceIntent = new Intent(this, LocationMonitorService.class);
+    	this.stopService(mServiceIntent);
+    	
+    	// update buttons visibility
+    	this.setServiceButtons(false);
+    }
 	
+	/**
+	 * Adds entries to context menu
+	 */
 	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
 		if (v.getId() == R.id.gridView1) {
 			AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
@@ -127,6 +187,7 @@ public class MainActivity extends Activity implements OnItemLongClickListener {
 			
 			menu.setHeaderTitle(s.name);
 			menu.add(Menu.NONE, CONTEXT_ID_DE_ACTIVATE, 1, s.active? getString(R.string.deactivate): getString(R.string.activate));
+			menu.add(Menu.NONE, CONTEXT_ID_EDIT, 1, getString(R.string.edit));
 			menu.add(Menu.NONE, CONTEXT_ID_DELETE, 2, getString(R.string.delete));
 		}
 	}
@@ -135,21 +196,59 @@ public class MainActivity extends Activity implements OnItemLongClickListener {
 		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
 		Station station = this.stationAdapter.getItemByPosition(info.position);
 		
-		if (CONTEXT_ID_DE_ACTIVATE == item.getItemId())
-		{
+		switch (item.getItemId()) {
+		case CONTEXT_ID_DE_ACTIVATE:
 			// (De)activate alarm
 			station.active = !station.active;
 			this.stationManager.save(station);
-		}
-		else if (CONTEXT_ID_DELETE == item.getItemId())
-		{
+			break;
+		case CONTEXT_ID_DELETE:
 			// Delete alarm
 			this.stationManager.delete(station._id);
+			break;
+		case CONTEXT_ID_EDIT:
+			Intent switchToAddStation = new Intent(this,
+					AddLocationActivity.class);
+			switchToAddStation.putExtra("stationId", station._id);
+			startActivityForResult(switchToAddStation, REQUEST_CODE_EDIT);
+			break;
+		default: // unkown code
+			return true;
 		}
 		
+		// Update grid view
+		this.updateGridView();
+    	
+		return true;
+	}
+	
+	/**
+	 * Updates the grid view with current stations
+	 */
+	private void updateGridView() {
 		this.stationAdapter.changeCursor(this.stationManager.getAllCursor());
 		this.stationAdapter.notifyDataSetChanged();
     	this.gridView.invalidate();
-		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_start:
+			this.startService();
+			break;
+		case R.id.button_stop:
+			this.stopService();
+			break;
+		default:
+
+		}
+	}
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> parent, View view,
+			int position, long id) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 };
